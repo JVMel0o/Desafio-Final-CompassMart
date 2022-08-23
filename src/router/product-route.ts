26a72a4c91ec61800e967b9a2aae537d9ff57b77
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-23 13:28:59 -0300

    feat: adding find by id product route

--- src/router/product-route.ts
+++ src/router/product-route.ts
@@ -6,5 +6,6 @@ const mainRoute = '/api/v1/product';
 
 router.post(`${mainRoute}`, productController.create);
 router.get(`${mainRoute}`, productController.findAll);
+router.get(`${mainRoute}/:id`, productController.findById);
 
 export default router;
