53b522810bd6298e45ea91da7152a7b80b81ae79
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-24 17:04:22 -0300

    feat: adding create product validation

--- src/api/models/schemas/productSchema.ts
+++ src/api/models/schemas/productSchema.ts
@@ -9,7 +9,7 @@ const ProductSchema = new Schema<IProduct>({
     brand: { type: String, required: true},
     price: { type: Number, min: 0.01, max: 1000.00, required: true },
     qtd_stock: { type: Number, max: 100000, required: true},
-    stock_control_enabled: { type: Boolean, required: true},
+    stock_control_enabled: { type: Boolean, required: false},
     bar_code: { type: String, minlength: 13, maxlenght: 13, required: true, unique: true}
 }, {
     timestamps: {
--- src/router/product-route.ts
+++ src/router/product-route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import productController from '../api/controllers/productController';
+import productCreateValidation from '../api/validation/products/product-create-validation';
 
 const router = Router();
 const mainRoute = '/api/v1/product';
 
-router.post(`${mainRoute}`, productController.create);
+router.post(`${mainRoute}`, productCreateValidation, productController.create);
 router.get(`${mainRoute}`, productController.findAll);
 router.get(`${mainRoute}/:id`, productController.findById);
 router.delete(`${mainRoute}/:id`, productController.delete);
