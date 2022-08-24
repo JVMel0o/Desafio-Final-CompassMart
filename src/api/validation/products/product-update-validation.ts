5252b117a085b1184140dd76518e97dbcc5fefca
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-24 17:23:23 -0300

    feat: adding update put product validation

---
+++ src/api/validation/products/product-update-validation.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import Joi from "joi";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const schema = Joi.object({
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            department: Joi.string().required(),
+            brand: Joi.string().required(),
+            price: Joi.number().required().min(0.01).max(1000),
+            qtd_stock: Joi.number().required().min(1).max(100000),
+            bar_code: Joi.string().required().length(13),
+        });
+
+        const { error } = await schema.validate(req.body, { abortEarly: true });
+        if (error) throw error;
+        return next();
+
+    } catch (error) { 
+        return res.status(400).json(error);
+    }
+};
--- src/router/product-route.ts
+++ src/router/product-route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import productController from '../api/controllers/productController';
 import productCreateValidation from '../api/validation/products/product-create-validation';
+import productUpdateValidation from '../api/validation/products/product-update-validation';
 
 const router = Router();
 const mainRoute = '/api/v1/product';
@@ -9,7 +10,7 @@ router.post(`${mainRoute}`, productCreateValidation, productController.create);
 router.get(`${mainRoute}`, productController.findAll);
 router.get(`${mainRoute}/:id`, productController.findById);
 router.delete(`${mainRoute}/:id`, productController.delete);
-router.put(`${mainRoute}/:id`, productController.update);
+router.put(`${mainRoute}/:id`, productUpdateValidation, productController.update);
 router.patch(`${mainRoute}/:id`, productController.update);
 
 export default router;
