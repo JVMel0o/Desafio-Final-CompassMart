68e3de5f6fd58922420473cdcccf9b9ea95d1536
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-25 11:49:45 -0300

    feat: adding invalid product id error case

---
+++ src/api/errors/products/productIdInvalid.ts
@@ -0,0 +1,11 @@
+class productIdInvalid {
+    public readonly message: string;
+    public readonly statusCode: number;
+
+    constructor () {
+        this.message = 'Product Id different from pattern.';
+        this.statusCode = 400;
+    }
+}
+
+export default productIdInvalid;
