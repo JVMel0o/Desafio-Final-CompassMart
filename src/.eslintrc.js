b9a1a9f6b0631c8ee3a502184feee0c8e4d9ffb4
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-27 00:56:00 -0300

    fix

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
       ecmaVersion: 'latest',
       sourceType: 'module'
     },
-    plugins: ['@typescript-eslint'],
+    plugins: [
+      '@typescript-eslint'
+    ],
     rules: {}
 }
