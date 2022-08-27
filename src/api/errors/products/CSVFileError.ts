9ce71358f2e5b45389208e2158ed4ee100f1d03b
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-26 22:10:59 -0300

    feat: adding csv file do not exists error

---
+++ src/api/errors/products/CSVFileError.ts
@@ -0,0 +1,11 @@
+class CSVFileError {
+    public readonly message: string;
+    public readonly statusCode: number;
+  
+    constructor () {
+      this.message = 'File CSV not found.';
+      this.statusCode = 404;
+    }
+  }
+  
+  export default CSVFileError;
