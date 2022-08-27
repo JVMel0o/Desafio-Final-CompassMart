d5a9f96f0d8729887c2bd719a3113faa90e3e982
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-08-26 22:41:24 -0300

    fix: hiding databse user infos

--- src/infra/database/mongo/index.ts
+++ src/infra/database/mongo/index.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 class Database {
   constructor () {
     this.connect()
   }
   connect () {
-    mongoose.connect(`mongodb+srv://1234:123@compassapi.q5yt5ki.mongodb.net/`);
+    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.q5yt5ki.mongodb.net/`);
     mongoose.connection.on('error', console.log.bind(console, 'Error connection.'));
     mongoose.connection.once('open', () => {
-      console.log('Connection with database.');
+      console.log('Connected with database.');
     });
     return mongoose.connection;
     }
