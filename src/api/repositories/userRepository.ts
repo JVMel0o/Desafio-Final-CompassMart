d099e87f37d8df1d6067d634101c14e02326e646
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-09-01 10:13:45 -0300

    feat: adding user repository

---
+++ src/api/repositories/userRepository.ts
@@ -0,0 +1,31 @@
+import { IUser, IUserAuthentication } from "../models/interfaces/userInterface";
+import userSchema from "../models/schemas/userSchema";
+import { PaginateResult } from "mongoose";
+import { IPaginate } from "../models/interfaces/paginateInterface";
+import userPaginate from "../../paginate/userPaginate";
+
+class UserRepository {
+    async create (payload: IUser): Promise<IUser | IUserAuthentication> {
+        return userSchema.create(payload)
+    }
+
+    async findAll (query: IPaginate): Promise<PaginateResult<IUser>> {
+        const options = {
+            page: query.page || 1,
+            limit: query.limit || 50,
+            customLabels: userPaginate
+        }
+        const users = await userSchema.paginate({}, options)
+        return users
+    }
+
+    async findByEmail (email: string): Promise<IUser | null> {
+        return userSchema.findOne({email});
+    }
+
+    async deleteByEmail (email: string): Promise<IUser | null> {
+        return userSchema.findOneAndDelete({email});
+    }
+}
+
+export default new UserRepository()
