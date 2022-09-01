0ef90d8661fda5cdc990bb66836f267e4e9ad666
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-09-01 11:50:15 -0300

    fix: missing / at main route and /:email at delete route

--- src/router/user-route.ts
+++ src/router/user-route.ts
@@ -2,11 +2,11 @@ import { Router } from 'express'
 import userController from '../api/controllers/userController'
 
 const router = Router()
-const mainRoute = 'api/v1/user'
+const mainRoute = '/api/v1/user'
 
 router
   .get(`${mainRoute}`, userController.findAll)
   .post(`${mainRoute}`, userController.create)
-  .delete(`${mainRoute}`, userController.deleteByEmail)
+  .delete(`${mainRoute}/:email`, userController.deleteByEmail)
 
 export default router
