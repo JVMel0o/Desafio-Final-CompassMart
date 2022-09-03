52d54ab970081884c5182deac67f6f7f640ba854
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-09-03 20:12:22 -0300

    feat: creating user email exists error case

---
+++ src/api/errors/users/UserEmailExists.ts
@@ -0,0 +1,11 @@
+class UserEmailExists {
+  public readonly message: string
+  public readonly statusCode: number
+
+  constructor () {
+    this.message = 'User Email is ALREADY registered.'
+    this.statusCode = 400
+  }
+}
+
+export default UserEmailExists
