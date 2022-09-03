e34beef898a0bb038f9c9799a3370b96d7aea111
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-09-03 19:04:12 -0300

    feat: creating user validation for post route

--- src/router/user-route.ts
+++ src/router/user-route.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import userController from '../api/controllers/userController'
+import userCreateValidation from '../api/validation/users/user-create-validation'
 
 const router = Router()
 const mainRoute = '/api/v1/user'
 
 router
   .get(`${mainRoute}`, userController.findAll)
-  .post(`${mainRoute}`, userController.create)
-  .post('/api/v1/authenticate', userController.authentication)
+  .post(`${mainRoute}`, userCreateValidation, userController.create)
+  .post('/api/v1/authenticate', userCreateValidation, userController.authentication)
   .delete(`${mainRoute}/:email`, userController.deleteByEmail)
 
 export default router
