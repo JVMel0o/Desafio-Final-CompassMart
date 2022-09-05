6f6829a89657fa296b0647127b1cb2770ae2b9fe
JVMel0o <joao.melo728@al.unieduk.com.br>
2022-09-04 22:06:00 -0300

    feat: adding user middleware to user route

--- src/router/user-route.ts
+++ src/router/user-route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import userController from '../api/controllers/userController'
 import userCreateValidation from '../api/validation/users/user-create-validation'
+import userAuthentication from '../middlewares/user-authentication'
 
 const router = Router()
 const mainRoute = '/api/v1/user'
@@ -9,6 +10,6 @@ router
   .get(`${mainRoute}`, userController.findAll)
   .post(`${mainRoute}`, userCreateValidation, userController.create)
   .post('/api/v1/authenticate', userCreateValidation, userController.authentication)
-  .delete(`${mainRoute}/:email`, userController.deleteByEmail)
+  .delete(`${mainRoute}/:email`, userAuthentication, userController.deleteByEmail)
 
 export default router
